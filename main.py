51de073cb94575f03612646d4802ebe6784ebc98
sds <163600875+justssd@users.noreply.github.com>
2025-01-26 08:16:02 -0800

    Add FRAMERATE logic

--- main.py
+++ main.py
@@ -2,15 +2,22 @@ import pygame
 
 from constants import *
 
+FRAMERATE = 60
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.Surface.fill(screen, BLACK)
         pygame.display.flip()
+        dt = clock.tick(FRAMERATE) / 1000
+
 
 if __name__ == "__main__":
     main()
